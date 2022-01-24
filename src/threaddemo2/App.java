e96bf836125a0b5d0c6299274c92cc654a619f42
Janardhan Pulivarthi <j143@protonmail.com>
2022-01-24 10:20:07 +0530

    run Runner in threads

--- src/threaddemo2/App.java
+++ src/threaddemo2/App.java
@@ -1,8 +1,8 @@
+package threaddemo2;
 
 class Runner extends Thread {
-    @Override
+
     public void run() {
-        super.run();
         for(int i=0; i<10; i++) {
             System.out.println("Hello" + i);
         }
@@ -11,7 +11,6 @@ class Runner extends Thread {
 
 public class App {
 
-    public void main(String[] args) {
-        //
+    public static void main(String[] args) {
     }
 }
