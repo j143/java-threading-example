c557442f1ae958ad477b36f25b83e6c785908af6
Janardhan Pulivarthi <j143@protonmail.com>
2022-01-24 12:42:00 +0530

    use synchronized lock Object

--- src/syncdemo2/App.java
+++ src/syncdemo2/App.java
@@ -3,6 +3,10 @@ package syncdemo2;
 public class App {
 
     private int count = 0;
+
+    // https://docs.oracle.com/javase/tutorial/essential/concurrency/locksync.html
+    private Object lock1 = new Object();
+
     public static void main(String[] args) {
         App app = new App();
         app.doSomething();
@@ -12,15 +16,34 @@ public class App {
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for(int i=0; i<10000; i++) {
-                    count = count + 1;
+                synchronized (lock1) {
+                    for(int i=0; i<10000; i++) {
+                        // read `count`, increment by 1 and store in `count`
+                        count = count + 1;
+                    }
                 }
+
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lock1) {
+                    for(int i=0; i<10000; i++) {
+                        // read `count`, increment by 1 and store in `count`
+                        count = count + 1;
+                    }
+                }
+
             }
         });
 
         t1.start();
+        t2.start();
         try {
             t1.join();
+            t2.join();
         }
         catch (InterruptedException e) {
             e.printStackTrace();
