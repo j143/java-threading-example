cab57d905734724eaa2aaba51c01f74e0d061543
Janardhan Pulivarthi <j143@protonmail.com>
2022-01-25 12:53:18 +0530

    countdown latch example

---
+++ src/countdownlatches/App.java
@@ -0,0 +1,42 @@
+package countdownlatches;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+class Processor implements Runnable {
+    private CountDownLatch latch;
+
+    Processor(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Process started." + latch.toString());
+        try {
+            Thread.sleep(300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class App {
+
+    public static void main(String[] args) {
+        CountDownLatch latch1 = new CountDownLatch(3);
+
+        ExecutorService es = Executors.newFixedThreadPool(3);
+
+        for(int i=0; i<100; i++)
+            es.submit(new Processor(latch1));
+
+        try {
+            latch1.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
